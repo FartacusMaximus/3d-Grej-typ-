1278fba7f3dca784efa6bc813bb32a4ff5562ced
william.arvidsson <william.arvidsson@elev.ga.lbs.se>
2018-11-16 15:59:25 +0100

    Grej 3d

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    CharacterController cC;
+    public float speed = 5;
+    public float turnSpeed = 2;
+    // Use this for initialization
+    void Start()
+    {
+        cC = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertInput = Input.GetAxis("Vertical");
+        float horizInput = Input.GetAxis("Horizontal");
+
+        cC.SimpleMove(transform.forward * vertInput*speed);
+        
+        transform.Rotate(Vector3.up*horizInput*turnSpeed);
+    }
+}
